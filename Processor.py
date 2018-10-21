Fix ndvi calculation

Processor.py
@@ -6,6 +6,7 @@ import json
 import cv2 as cv
 from collections import defaultdict
 import gdal
+import tifffile
 
 
 green_zones = {
@@ -153,6 +154,42 @@ class Processor:
         mask = cv.inRange(converted_image, green_zone["lower_bound"], green_zone["upper_bound"])
         return mask
 
+    def get_ndvi_for_city(self, city):
+        if city["NAME"].values[0] in self.cities_info.keys() and \
+                "ndvi_raster_filename" in self.cities_info[city["NAME"].values[0]].keys():
+            return self.cities_info["ndvi_raster_filename"]
+
+        bb = self.find_bb_for_city(city)
+        centroid = city["geometry"].centroid
+
+        curr_starttime, curr_endtime = self.get_start_end_time(centroid)
+
+        fc = self.metadata_client.search(products="modis:09:CREFL", geom=bb, start_time=curr_starttime,
+                                         end_time=curr_endtime, limit=1)
+
+        band_info = self.metadata_client.get_band("modis:09:CREFL:ndvi")
+        physical_range = band_info['physical_range']
+        data_range = band_info['data_range']
+
+        feat_ids = [feat['id'] for feat in fc['features']]
+
+        arr, meta = self.raster_client.ndarray(
+            feat_ids,
+            cutline=bb,
+            bands=['ndvi', 'alpha'],
+            scales=[[data_range[0], data_range[1], physical_range[0], physical_range[1]], None],
+            data_type='Float32',
+            resolution=15)
+
+        nodata = arr[:, :, -1] == 0
+        masked = np.where(nodata, 0, arr[:, :, 0])
+        
+        tifffile.imsave("data/images/" + city["NAME"].values[0] + "_NDVI" + ".tif", masked)
+        self.cities_info[city["NAME"].values[0]]["ndvi_raster_filename"] = "data/images/" + city["NAME"].values[0] + \
+                                                                           "_NDVI" + ".tif"
+        self.save_database()
+        return self.cities_info[city["NAME"].values[0]]["ndvi_raster_filename"]
+
     def get_ndvi_raster_for_city(self, city):
         if city["NAME"].values[0] in self.cities_info.keys() and \
                 "ndvi_raster_filename" in self.cities_info[city["NAME"].values[0]].keys():
@@ -177,8 +214,7 @@ class Processor:
                                   save=True,
                                   outfile_basename="data/images/" + city["NAME"].values[0] + "_NDVI",
                                   resolution=15)
-        self.cities_info[city["NAME"].values[0]]["ndvi_raster_filename"] = "data/images/" + city["NAME"].values[0] + \
-                                                                           "_NDVI" + ".tif"
+        self.cities_info[city["NAME"].values[0]]["ndvi_raster_filename"] = "data/images/" + city["NAME"].values[0] + "_NDVI" + ".tif"
         return self.cities_info[city["NAME"].values[0]]["ndvi_raster_filename"]
 
     @staticmethod
@@ -222,14 +258,15 @@ class Processor:
         mask_based_on_color = self.mask_image_based_on_color(raster_image, green_zone)
 
         if "ndvi_raster_filename" not in self.cities_info[city_name]:
-            ndvi_raster_filename = self.get_ndvi_raster_for_city(city)
+            ndvi_raster_filename = self.get_ndvi_for_city(city)
         else:
             ndvi_raster_filename = self.cities_info[city_name]["ndvi_raster_filename"]
 
         ds = gdal.Open(ndvi_raster_filename)
+        print(ndvi_raster_filename)
         ndvi_raster_image = np.array(ds.GetRasterBand(1).ReadAsArray())
 
-        print(np.mean(ndvi_raster_image))
+        print(np.max(ndvi_raster_image))
 
         mask_based_on_ndvi = self.mask_image_based_on_ndvi(ndvi_raster_image, green_zone)
 
@@ -237,38 +274,8 @@ class Processor:
         cv.imwrite("data/images/" + city_name + green_zone_name + ".png", combined_mask)
         self.cities_info[city_name][green_zone_name] = "data/images/" + city_name + green_zone_name + ".png"
         self.save_database()
-        
+
         return combined_mask
-
-    def test_ndvi(self, city):
-        aoi = self.find_bb_for_city(city)
-        # Request Modis imagery, which contains indicies that need to be scaled
-        fc = self.metadata_client.search(products="modis:09:CREFL", geom=aoi, start_time="2017-05-01",
-                                    end_time="2018-05-15", limit=1)
-
-        # Fetch the band information using the Metadata API, including the NDVI ranges
-        band_info = self.metadata_client.get_band("modis:09:CREFL:ndvi")
-        physical_range = band_info['physical_range']
-        data_range = band_info['data_range']
-
-        # Isolate the image IDs to pull data for
-        feat_ids = [feat['id'] for feat in fc['features']]
-
-        # Request the NDVI band and scale it accordingly and the alpha band for masking next
-        arr, meta = self.raster_client.ndarray(
-            feat_ids,
-            cutline=aoi,
-            bands=['ndvi', 'alpha'],
-            scales=[[data_range[0], data_range[1], physical_range[0], physical_range[1]], None],
-            data_type='Float32',
-            resolution=15)
-
-        # mask out nodata pixels
-        nodata = arr[:, :, -1] == 0
-        masked = np.where(nodata, 0, arr[:, :, 0])
-
-        print(np.max(arr))
-        print(np.mean(arr))
 
     def save_database(self):
         with open(self.path_to_cities_info, 'w') as outfile: